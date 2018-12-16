7711eee00a7f13a37afbdf9c651262ad0bd87a3e
liweirong <2277839278@qq.com>
2018-12-16 22:02:44 +0800

    spark streaming-kafka

--- src/main/java/com/iris/streaming/ReceiverFromKafka.scala
+++ src/main/java/com/iris/streaming/ReceiverFromKafka.scala
@@ -7,17 +7,19 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import com.alibaba.fastjson.JSON
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
 
 object ReceiverFromKafka {
 
   def main(args: Array[String]): Unit = {
-    if (args.length < 4) {
-      System.err.println("Usage: HdfsWordCount <directory>")
-      System.exit(1)
-    }
-
-    val Array(group_id, topic, exectime, dt) = args
+    //    if (args.length < 4) {
+    //      System.err.println("Usage: HdfsWordCount <directory>")
+    //      System.exit(1)
+    //    }
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    //    val Array(group_id, topic, exectime, dt) = args
+    val Array(group_id, topic, exectime, dt) = Array("group_test", "test", "20", "")
     val zkHostIP = Array("10", "11", "12").map("192.168.171." + _)
     val ZK_QUORUM = zkHostIP.map(_ + ":2181").mkString(",")
     //192.168.174.134:2181,192.168.174.125:2181,192.168.174.129:2181
@@ -35,32 +37,33 @@ object ReceiverFromKafka {
     // 为什么是map（_._2）: @return DStream of (Kafka message key, Kafka message value)
     val mesR = KafkaUtils.createStream(ssc, ZK_QUORUM, group_id, topicMap).map(_._2)
 
-    def rdd2DF(rdd: RDD[String]): DataFrame = {
-      val spark = SparkSession
-        .builder()
-        .appName("Streaming Frome Kafka")
-        .config("hive.exec.dynamic.partition", "true") // 动态分区
-        .config("hive.exec.dynamic.partition.mode", "nonstrict")
-        .enableHiveSupport().getOrCreate()
-
-      // 涉及隐式转换
-      import spark.implicits._
-      // python去写 - > Json格式的数据 ??  {"order_id":"1","user_id":"2"}   ??
-      case class Order(order_id: String, user_id: String)
-
-      rdd.map { x =>
-        val mess = JSON.parseObject(x, classOf[Orders])
-        Order(mess.order_id, mess.user_id)
-      }.toDF()
-
-    }
-
-    val log = mesR.foreachRDD { rdd =>
-      val df = rdd2DF(rdd)
-      df.withColumn("dt", lit(dt.toString))
-        .write.mode(SaveMode.Append)
-        .insertInto("bigData.order_partition") //数据追加到这个表中
-    }
+    mesR.map((_, 1L)).reduceByKey(_ + _).print
+    //    def rdd2DF(rdd: RDD[String]): DataFrame = {
+    //      val spark = SparkSession
+    //        .builder()
+    //        .appName("Streaming Frome Kafka")
+    //        .config("hive.exec.dynamic.partition", "true") // 动态分区
+    //        .config("hive.exec.dynamic.partition.mode", "nonstrict")
+    //        .enableHiveSupport().getOrCreate()
+    //
+    //      // 涉及隐式转换
+    //      import spark.implicits._
+    //      // python去写 - > Json格式的数据 ??  {"order_id":"1","user_id":"2"}   ??
+    //      case class Order(order_id: String, user_id: String)
+    //
+    //      rdd.map { x =>
+    //        val mess = JSON.parseObject(x, classOf[Orders])
+    //        Order(mess.order_id, mess.user_id)
+    //      }.toDF()
+    //
+    //    }
+    //
+    //    val log = mesR.foreachRDD { rdd =>
+    //      val df = rdd2DF(rdd)
+    //      df.withColumn("dt", lit(dt.toString))
+    //        .write.mode(SaveMode.Append)
+    //        .insertInto("bigData.order_partition") //数据追加到这个表中
+    //    }
 
     ssc.start()
     ssc.awaitTermination()
--- src/main/java/com/iris/streaming/test.scala
+++ src/main/java/com/iris/streaming/test.scala
@@ -7,42 +7,43 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 /**
   * 8 sparkStreaming
   *
-  *  在192.168.171.10设备上输入 ]# nc -lp 9999  输入后回车即可与此连接
+  * 在192.168.171.10设备上输入 ]# nc -lp 9999  输入后回车即可与此连接
   */
 object test {
   def main(args: Array[String]): Unit = {
+    // Create a local StreamingContext with two working thread and batch interval of 2 second.
+    // The master requires 2 cores to prevent from a starvation scenario.
     val conf = new SparkConf().setAppName("WC").setMaster("local[2]")
-    val sc = new StreamingContext(conf,Seconds(2))
+    val sc = new StreamingContext(conf, Seconds(2))
     Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
 
-    val lines = sc.socketTextStream("192.168.171.10",9999)
+    // Create a DStream that will connect to hostname:port, like localhost:9999
+    val lines = sc.socketTextStream("192.168.171.10", 9999)
+    // Split each line into words
     val words = lines.flatMap(_.split(" "))
-//    val wordCounts = words.map((_,1)).reduceByKey(_+_)
-//    wordCounts.print()
-//    //1 这种方法每两秒都会输出，没有输入就没有输出
+    //    val wordCounts = words.map((_,1)).reduceByKey(_+_)
+    //    wordCounts.print()
+    //    //1 这种方法每两秒都会输出，没有输入就没有输出
 
 
-//    val wordCounts2 = words.map((_,1)).reduceByKeyAndWindow((a:Int,b:Int)=>a+b,Seconds(20),Seconds(2)) // 必须是倍数
-//    wordCounts2.print()
-// 2 窗口滑动  有些数据在里面存放20秒
+    //    val wordCounts2 = words.map((_,1)).reduceByKeyAndWindow((a:Int,b:Int)=>a+b,Seconds(20),Seconds(2)) // 必须是倍数
+    //    wordCounts2.print()
+    // 2 窗口滑动  有些数据在里面存放20秒
 
 
     sc.checkpoint("D:\\workspace\\idea_workspace\\git_project\\sparkStreamingTmp")
-    val addFunc=(curValues:Seq[Long],preValueState:Option[Long])=>{
+    val addFunc = (curValues: Seq[Long], preValueState: Option[Long]) => {
       val curCount = curValues.sum // 当前两秒的批次求和
-      val preCount = preValueState.getOrElse(0L)
-      Some(curCount+ preCount)
+      val preCount = preValueState.getOrElse(0L) // 存入内存中
+      Some(curCount + preCount)
     }
-    val wordCounts3 = words.map((_,1L)).updateStateByKey[Long](addFunc)
+    val wordCounts3 = words.map((_, 1L)).updateStateByKey[Long](addFunc)
     wordCounts3.print()
 
-    //必须设置checkPoint，不然报错 ：java.lang.IllegalArgumentException: requirement failed: The checkpoint directory has not been set. Please set it by StreamingContext.checkpoint().
+    //updateStateByKey 全局统计量 必须设置checkPoint，不然报错 ：java.lang.IllegalArgumentException: requirement failed: The checkpoint directory has not been set. Please set it by StreamingContext.checkpoint().
 
-    sc.start()
-    sc.awaitTermination()
-
-
-
+    sc.start() // Start the computation
+    sc.awaitTermination() // Wait for the computation to terminate
 
 
   }
